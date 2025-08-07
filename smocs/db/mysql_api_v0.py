fix batch size calculation for db

smocs/db/mysql_api_v0.py
@@ -273,8 +273,8 @@ class DBManager:
                 
                 for key in batch:
                     batch[key].append([results[i][key] for i in range(len(results))])
-                           
-            required_samples = required_samples - len(batch)
+            key = list(batch.keys())[0]
+            required_samples = required_samples - len(batch[key])
             
         for key in batch:
             batch[key] = np.array(batch[key])