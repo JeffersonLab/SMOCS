46cc8cc4aaddfe946e4184fb812bb35b291cc308
Kishanrajput <kishansingh0817@gmail.com>
2025-08-11 10:39:55 -0400

    make query execution functions private in api and document all the functions

--- smocs/db/mysql_api_v0.py
+++ smocs/db/mysql_api_v0.py
@@ -31,7 +31,7 @@ class DBManager:
             db_cursor (MySQLCursor): A cursor object for executing SQL queries on the database.
 
         Example:
-            >>> db_connector = DBConnector("db_config.yaml")
+            >>> db_connector = DBManager("db_config.yaml")
             Initializing DBConnector
             Connected to DB: my_database
         """
@@ -51,7 +51,7 @@ class DBManager:
         return self.mydb.is_connected()
         
 
-    def execute_and_commit(self, query, values=None):
+    def __execute_and_commit(self, query, values=None):
         """
         Executes a SQL query and commits the changes to the database.
 
@@ -80,7 +80,7 @@ class DBManager:
         
         return status
     
-    def execute_query(self, query):
+    def __execute_query(self, query):
         """
         Executes a SQL query and returns the results.
 
@@ -104,7 +104,18 @@ class DBManager:
         return results
 
     def connect(self, db_config, n_connection_trials=5):
+
         """
+        Establishes a connection to the MySQL database using the provided configuration.
+        Args:
+            db_config (dict): A dictionary containing the database connection parameters such as host, user,
+                                password, and database name.
+            n_connection_trials (int): The number of attempts to connect to the database before giving up.
+        Raises:
+            Exception: If the connection fails after the specified number of trials, an exception is raised and
+                        the process is terminated.
+        Returns:
+            None
         """
         
         for i in range(n_connection_trials):
@@ -130,12 +141,22 @@ class DBManager:
 
 
     def create_tables(self):
+        """
+        Creates the necessary tables in the database if they do not already exist.
+        This method is called during the initialization of the DBManager to ensure that the required tables
+        for storing agent information, inferences, and replay data are present in the database.
+        It executes SQL queries to create the following tables:
+            - agent_information: Stores information about the agents.
+            - agent_inferences: Stores the inferences made by the agents.
+            - agent_replay: Stores the replay data for the agents.
+        If the database does not exist, it will be created first.
+        """
         query = f"CREATE DATABASE IF NOT EXISTS {self.db_name}"
-        self.execute_and_commit(query)
+        self.__execute_and_commit(query)
         self.mydb.database = self.db_name
 
         query = f"CREATE TABLE IF NOT EXISTS agent_information (id INT AUTO_INCREMENT PRIMARY KEY, registered_id VARCHAR(50) NOT NULL, agent_name VARCHAR(50), config BLOB, info BLOB)"
-        self.execute_and_commit(query)
+        self.__execute_and_commit(query)
 
         query = """
         CREATE TABLE IF NOT EXISTS agent_inferences (id INT AUTO_INCREMENT PRIMARY KEY, 
@@ -145,7 +166,7 @@ class DBManager:
                                                                 prediction_timestamp DATETIME(6), 
                                                                 prediction BLOB)
         """
-        self.execute_and_commit(query)
+        self.__execute_and_commit(query)
 
         query = """
         CREATE TABLE IF NOT EXISTS agent_replay (
@@ -162,10 +183,25 @@ class DBManager:
                                                 FOREIGN KEY (state_id) REFERENCES agent_inferences(id)
                                             )
         """
-        self.execute_and_commit(query)
+        self.__execute_and_commit(query)
 
     
     def parse_results(self, results):
+        """
+        Parses the results from the database query, converting any decimal.Decimal or bytes types to appropriate formats.
+        This function is used to ensure that the data retrieved from the database is in a format that can be easily used in Python, such as converting decimal.Decimal to float and bytes to numpy arrays or unpickled objects.
+        Args:
+            results (list): A list of dictionaries containing the results from the database query.
+        Returns:
+            np.ndarray: A numpy array containing the parsed results, with decimal.Decimal converted to float and bytes converted to numpy arrays or unpickled objects.
+        Raises:
+            None: If the results are empty or if there are no decimal.Decimal or bytes types in the results, it simply returns an empty numpy array.
+        Example:
+            >>> results = [{'id': 1, 'value': decimal.Decimal('3.14'), 'data': b'\x00\x01\x02\x03'}]
+            >>> parsed_results = self.parse_results(results)
+            >>> print(parsed_results)
+            [{'id': 1, 'value': 3.14, 'data': array([0., 1., 2., 3.])}]
+        """
         parsed_results = []
         for result in results:
             for key in result:
@@ -184,6 +220,23 @@ class DBManager:
         return np.array(parsed_results)
 
     def get_timestamps(self, window_size, mode="random", n=1):
+        """
+        Retrieves a list of timestamps from the database that have at least `window_size` number of records in the `agent_inferences` table.
+        Args:
+            window_size (int): The minimum number of records required for each timestamp.
+            mode (str): The mode of sampling, either "random" or "latest". Defaults to "random".
+            n (int): The number of timestamps to retrieve. Defaults to 1.
+        Returns:
+            list: A list of dictionaries containing the timestamps that meet the criteria.
+        Raises:
+            None: If the mode is not understood, it returns None.
+        Example:
+            >>> timestamps = self.get_timestamps(window_size=5, mode="random", n=3)
+            >>> print(timestamps)
+            [{'state_source_timestamp': '2023-10-01 12:00:00'}, 
+             {'state_source_timestamp': '2023-10-01 12:05:00'}, 
+             {'state_source_timestamp': '2023-10-01 12:10:00'}]
+        """
 
         query = f"""
         SELECT ai.state_source_timestamp
@@ -208,7 +261,7 @@ class DBManager:
             print("mode not understood in get_timestamps function...most likely coming from invalid mode argument to sample_batch function. Valid values are 'random' or 'latest' ")
             return None
         
-        results = self.execute_query(query)
+        results = self.__execute_query(query)
         parsed_results = self.parse_results(results)
 
         return parsed_results
@@ -216,7 +269,21 @@ class DBManager:
     
     def sample_sequence(self, window_time_seed, agent_type, segment_length):
         """
-        
+        Samples a sequence of data from the database starting from a given timestamp.
+        Args:
+            window_time_seed (str): The starting timestamp for the sequence.
+            agent_type (str): The type of agent for which the sequence is being sampled.
+            segment_length (int): The length of the segment to be sampled.
+        Returns:
+            list: A list of dictionaries containing the sampled data for the specified agent type.
+        Raises:
+            Exception: If there is an error executing the query, it will print the error message and return None.
+        Example:
+            >>> sequence = self.sample_sequence(window_time_seed='2023-10-01 12 :00:00', agent_type='diagnostics', segment_length=10)
+            >>> print(sequence)
+            [{'state_source_timestamp': '2023-10-01 12:00:00', 'state': array([0.1, 0.2])},
+             {'state_source_timestamp': '2023-10-01 12:00:01', 'state': array([0.3, 0.4])},
+             ...]
         """
         if agent_type.lower() != "controls":
             # query = f"SELECT ai.state_source_timestamp, ai.state, ar.next_state FROM agent_inferences ai JOIN agent_replay ar ON ai.Id = ar.state_id WHERE ai.state_source_timestamp >= '{window_time_seed}' ORDER BY ai.state_source_timestamp LIMIT {segment_length}"
@@ -246,6 +313,13 @@ class DBManager:
 
         Returns:
             bool: True if there are enough samples, False otherwise.
+
+        Raises:
+            None: If the agent_type is not recognized, it returns False.
+        Example:
+            >>> success = self.check_sample_feasibility(segment_length=10, agent_type='diagnostics')
+            >>> print(success)
+            True
         """
         success = True
         number_of_records_prediction_table = self.get_size(table_name="agent_inferences")
@@ -265,7 +339,21 @@ class DBManager:
     def sample_batch(self, batch_size, segment_length, agent_type, mode="random"):
         # Select n random timestamps as starting point for sequences
         """
+        Samples a batch of data from the database based on the specified parameters.
 
+        Args:
+            batch_size (int): The number of samples to be included in the batch.
+            segment_length (int): The length of each segment to be sampled.
+            agent_type (str): The type of agent for which the batch is being sampled. Valid
+                values are 'controls' or 'diagnostics'.
+            mode (str): The mode of sampling, either 'random' or 'latest'. Defaults to 'random'.   
+
+
+        Raises:
+            ValueError: If the agent_type is not 'controls' or 'diagnostics', or if there are not enough samples in the database to sample a batch.
+
+        Returns:
+            dict: A dictionary containing the sampled batch data, with keys such as 'state_source_timestamp', 'state', 'prediction', 'next_state', 'reward', 'terminate', and 'truncate'.
         """
         if agent_type.lower() not in ["controls", "diagnostics"]:
             print(f"Invalid agent_type: {agent_type}. Valid values are 'controls' or 'diagnostics'.")
@@ -315,6 +403,18 @@ class DBManager:
 
     
     def record_sensor_data(self, data):
+        """
+        Records sensor data into the database.
+
+        Args:
+            data (dict): A dictionary containing the sensor data to be stored. The keys should match the columns in the `agent_inferences` table.
+
+        Returns:
+            int: The status of the operation, 0 if successful, 1 if there was an error.
+
+        Raises:
+            AssertionError: If the data is not a dictionary or if it is empty.
+        """
         assert isinstance(data, dict), "Data must be a dictionary"
         if len(data) == 0:
             print("No data to store, exiting...")
@@ -333,7 +433,7 @@ class DBManager:
             values.append(data[key]) 
         query += query_columns[:-2] + ") VALUES " + query_values[:-2] + ")"
         
-        status = self.execute_and_commit(query, values=tuple(values)) 
+        status = self.__execute_and_commit(query, values=tuple(values)) 
             
         return status
     
@@ -348,7 +448,7 @@ class DBManager:
             int: The state ID corresponding to the given source timestamp.
         """
         query = f"SELECT id FROM agent_inferences WHERE state_source_timestamp = '{source_timestamp}'"
-        results = self.execute_query(query)
+        results = self.__execute_query(query)
         
         if len(results) == 0:
             print(f"No state found for source timestamp: {source_timestamp}")
@@ -360,17 +460,46 @@ class DBManager:
         return int(results[0]['id'])
     
     def record_prediction(self, prediction, prediction_timestamp, key_value, key="state_source_timestamp"):
+        """
+        Records a prediction in the database.
+
+        Args:
+            prediction (np.ndarray): The prediction data to be stored.
+            prediction_timestamp (str): The timestamp of the prediction.
+            key_value (str): The value of the key to identify the record to update.
+            key (str): The key to identify the record, either 'state_source_timestamp' or 'state_id'. Defaults to 'state_source_timestamp'.
+
+        Returns:
+            int: The status of the operation, 0 if successful, 1 if there was an error.
+        Raises:
+            AssertionError: If the prediction is not a numpy array or if the key is not one of 'state_source_timestamp' or 'state_id'.
+        """
         assert isinstance(prediction, np.ndarray), "Prediction must be a numpy array"
         assert key in ['state_source_timestamp', 'state_id'], f"Key must be one of 'state_source_timestamp' or 'state_id', got {key}"
         
         query = f"UPDATE agent_inferences set prediction = %s, prediction_timestamp = '{prediction_timestamp}' WHERE {key} = %s"
         values = (prediction.tostring(), key_value)
         
-        status = self.execute_and_commit(query, values=values)    
+        status = self.__execute_and_commit(query, values=values)    
             
         return status
     
     def record_controls_tuple(self, data, state_id):
+        """
+        Records a controls tuple in the database.
+
+        Args:
+            data (dict): A dictionary containing the controls tuple data to be stored. 
+                         It must contain keys such as 'next_state', 'reward', 'terminate', and 'truncate'.
+            state_id (int): The ID of the state to which this controls tuple belongs.
+
+        Returns:
+            int: The status of the operation, 0 if successful, 1 if there was an error.
+
+        Raises:
+            AssertionError: If the data is not a dictionary or if it does not contain the required keys.
+            AssertionError: If the state_id is None, indicating that the controls tuple cannot be stored without a valid state ID.
+        """
         assert isinstance(data, dict), "controls tuple must be passed as a dictionary"
         assert 'next_state' in data, "controls tuple must contain 'next_state'"
         assert 'reward' in data, "controls tuple must contain 'reward'"
@@ -398,11 +527,20 @@ class DBManager:
         query += query_columns[:-2] + ") VALUES " + query_values[:-2] + ")"
         
         
-        status = self.execute_and_commit(query, values=tuple(values))
+        status = self.__execute_and_commit(query, values=tuple(values))
         
         return status
     
     def get_size(self, table_name):
+        """
+        Returns the number of records in the specified table.
+
+        Args:
+            table_name (str): The name of the table for which the record count is to be retrieved.
+
+        Returns:
+            int: The number of records in the specified table.
+        """
         query = f"SELECT COUNT(*) FROM {table_name}"
         self.db_cursor.execute(query)
         rowcount = self.db_cursor.fetchone()
