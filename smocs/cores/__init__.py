876caca9389f56cb11760ed2ae8140a6f1044dd6
Armen Kasparian <armenk@jlab.org>
2025-08-28 12:30:17 -0400

    Initial implementation of the ml inference thread base class

--- smocs/cores/__init__.py
+++ smocs/cores/__init__.py
@@ -5,6 +5,7 @@ Core components for SMOCS Kafka streaming system.
 from .kafka_producer_base import KafkaProducerBase
 from .kafka_consumer_base import KafkaConsumerBase
 from .kafka_streaming_process_base import KafkaStreamingProcessBase
+from .ml_inference_thread_base import MLInferenceThreadBase
 
 __version__ = "1.0.0"
-__all__ = ["KafkaProducerBase", "KafkaConsumerBase", "KafkaStreamingProcessBase"]
+__all__ = ["KafkaProducerBase", "KafkaConsumerBase", "KafkaStreamingProcessBase", "MLInferenceThreadBase"]
