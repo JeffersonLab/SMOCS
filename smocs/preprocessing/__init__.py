91e573326cb0dc177f4ecdaacf43b5e39d376a85
Armen Kasparian <armenk@jlab.org>
2025-09-29 11:53:59 -0400

    Preprocessing cannot be done lazily as the modules need to be registered via the registation found on the bottom of the files for them to be used in the agent. Switched the preprocessing back to eager

--- smocs/preprocessing/__init__.py
+++ smocs/preprocessing/__init__.py
@@ -1,5 +1,14 @@
 """Preprocessing modules for SMOCS."""
 
+# Import registry first
+from .registry import PreprocessingRegistry
+from .manager import PreprocessingManager
+from .base import BasePreprocessor
+
+# EAGERLY import and register all processors
+from .window_processor import WindowProcessor
+from .bounds_normalizer import BoundsNormalizer
+
 __all__ = [
     'PreprocessingRegistry',
     'PreprocessingManager', 
@@ -7,21 +16,3 @@ __all__ = [
     'WindowProcessor',
     'BoundsNormalizer'
 ]
-
-def __getattr__(name):
-    if name == "PreprocessingRegistry":
-        from .registry import PreprocessingRegistry
-        return PreprocessingRegistry
-    elif name == "PreprocessingManager":
-        from .manager import PreprocessingManager
-        return PreprocessingManager
-    elif name == "BasePreprocessor":
-        from .base import BasePreprocessor
-        return BasePreprocessor
-    elif name == "WindowProcessor":
-        from .window_processor import WindowProcessor
-        return WindowProcessor
-    elif name == "BoundsNormalizer":
-        from .bounds_normalizer import BoundsNormalizer
-        return BoundsNormalizer
-    raise AttributeError(f"module {__name__!r} has no attribute {name!r}")
