36094bc2c85c1f167e13a306b94646d05d28f4d4
torri <roark@jlab.org>
2026-08-27 12:12:07 -0400

    more randoms

--- smocs/producers/__init__.py
+++ smocs/producers/__init__.py
@@ -2,7 +2,7 @@
 Producer implementations for SMOCS.
 """
 
-__all__ = ["MQTTKafkaProducer", "EpicsKafkaProducer"]
+__all__ = ["MQTTKafkaProducer", "EpicsKafkaProducer", "RootFileKafkaProducer"]
 
 def __getattr__(name):
     if name == "MQTTKafkaProducer":
@@ -11,4 +11,7 @@ def __getattr__(name):
     elif name == "EpicsKafkaProducer":
         from .epics_kafka_producer import EpicsKafkaProducer
         return EpicsKafkaProducer
+    elif name == "RootFileKafkaProducer":
+        from .root_file_kafka_producer import RootFileKafkaProducer
+        return RootFileKafkaProducer
     raise AttributeError(f"module {__name__!r} has no attribute {name!r}")
